0489f8e99c7eae196ed23f3588fcfc8e036fbae1
Arya <starmansix@gmail.com>
2017-04-05 16:26:17 -0500

    Comments are good

--- core/auth/credentials.js
+++ core/auth/credentials.js
@@ -1,7 +1,12 @@
 const bcrypt    = require('bcrypt');
 const validator = require('validator');
 
-exports.encrypt = password => new Promise((resolve, reject) => {
+/**
+ * Hash password with added salt using bcrypt.
+ *
+ * @param {String} password - The password to hash
+ */
+exports.encrypt = (password) => new Promise((resolve, reject) => {
   bcrypt.genSalt(10, (err, salt) => {
     if (err) reject(err);
 
@@ -11,14 +16,31 @@ exports.encrypt = password => new Promise((resolve, reject) => {
   });
 })
 
+/**
+ * Compare plain text password to hashed password using bcrypt.
+ * If the passwords match, the value match will resolve to 'true'.
+ *
+ * @param {String} password - The plain text password
+ */
 exports.compare = (candidate, password) => new Promise((resolve, reject) => {
   bcrypt.compare(candidate, password, (err, match) => {
     err ? reject(err) : resolve(match);
   });
 })
 
-exports.validateEmail = email => validator.isEmail(email)
+/**
+ * Check if given email is a valid email using
+ * the validator package.
+ *
+ * @param {String} email - The email to validate
+ */
+exports.validateEmail = (email) => validator.isEmail(email)
 
-exports.normalizeEmail = email => validator.normalizeEmail(email, {
+/**
+ * Normalize the given email address.
+ *
+ * @param {String} email - The email to normalize
+ */
+exports.normalizeEmail = (email) => validator.normalizeEmail(email, {
   all_lowercase: true
 })
--- core/auth/index.js
+++ core/auth/index.js
@@ -1,9 +1,15 @@
 const passport = require('passport');
 
+/**
+ * Define jwt authentication middleware.
+ */
 exports.requireAuth = passport.authenticate('jwt', {
   session: false
 })
 
+/**
+ * Define login required middleware.
+ */
 exports.requireLogin = passport.authenticate('local', {
   session: false
 })
